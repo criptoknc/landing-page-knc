e45cc6b67af1a6ceca74d727e86c7cc6adfedf0f
criptoknc <78485524+criptoknc@users.noreply.github.com>
2024-11-01 11:12:29 -0300

    agregue un loading spiner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../components/home.css';
 import fondo from '../images/job-collaboration-design-event-office-white-collar-worker-1560411-pxhere.com.jpg';
 
 const Home = () => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
   return (
     <section id="inicio">
       <div className="image-container">
-        <img src={fondo} alt="trabajo" />
-        <div className="overlay"></div>
-        <div className="text-overlay">
-          <p className="titulo">Hacemos crecer tu negocio en línea</p>
-          <p className="explicar">
-            Si estás buscando una agencia que te ayude a crear una presencia notable en línea, estás en el lugar correcto.
-          </p>
-          <div className='contenedor-boton'>
-          <a
-              id="whatsappLink"
-              className="enviar-mensaje"
-              href="https://wa.me/5493516862090" // Reemplaza con tu número
-              target="_blank"
-              rel="noopener noreferrer" // Agrega este atributo
-            >
-              Enviar un mensaje
-            </a>
-          </div>
-        </div>
+        {!isImageLoaded && <div className="spinner"></div>}
+        
+        <img 
+          src={fondo} 
+          alt="trabajo" 
+          onLoad={() => setIsImageLoaded(true)} 
+          style={{ display: isImageLoaded ? 'block' : 'none' }}
+        />
+        
+        {isImageLoaded && (
+          <>
+            <div className="overlay"></div>
+            <div className="text-overlay">
+              <p className="titulo">Hacemos crecer tu negocio en línea</p>
+              <p className="explicar">
+                Si estás buscando una agencia que te ayude a crear una presencia notable en línea, estás en el lugar correcto.
+              </p>
+              <div className="contenedor-boton">
+                <a
+                  id="whatsappLink"
+                  className="enviar-mensaje"
+                  href="https://wa.me/5493516862090"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Enviar un mensaje
+                </a>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
 };
 
 export default Home;
+
